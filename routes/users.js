401a19b4dfc78b91ffd2578602d96e3b828569d8
Akanksha-2611 <akankshatiwari5171@gmail.com>
2024-03-17 16:13:50 +0530

    changes

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const plm=require("passport-local-mongoose");
 
 mongoose.connect("mongodb://127.0.0.1:27017/Pinterest")
 
@@ -9,8 +10,7 @@ const userSchema = new mongoose.Schema({
         unique: true
     },
     password: {
-        type: String,
-        required: true
+        type: String
     },
     posts: [{
         type:mongoose.Schema.Types.ObjectId,
@@ -30,6 +30,7 @@ const userSchema = new mongoose.Schema({
     }
 });
 
+userSchema.plugin(plm)
 module.exports = mongoose.model('User', userSchema);
 
  
